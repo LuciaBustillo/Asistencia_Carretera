cc5c43adf54fcf35a42e2e9ff8d723e88e0a772a
LuciaBustillo <luchi_bo@hotmail.com>
2022-12-27 17:32:27 +0100

    Adding merge

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -11,18 +10,4 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './component/App.js';
-import { BrowserRouter } from "react-router-dom"
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
->>>>>>> 9bc30f075cd5175a0ac51d206a5e5642fe1663f9:src/index.js
 )
